40ecf2d5cc4f1a02950f1615add86cfbb6b348c9
Hadi-Fawad <hadi.fawad@ou.edu>
2023-02-15 22:01:12 -0600

    Made an error, accidently pulled old version and commited it. This is the correct reverse list

--- sweassignment3.py
+++ sweassignment3.py
@@ -3,24 +3,29 @@ def sum_list(num):
 
 def product_list(num):
     product = 1
-    
     for number in num:
         product *= number
     return product
+
+def reverse_list(num):
+    return num[::-1]
 
 def main():
 
     inputstring = input("Enter values in the array with a space in between.")
     inputnums = inputstring.split()
 
-    numbers = []
+    num = []
     for item in inputnums:
-        numbers.append(int(item))
+        num.append(int(item))
 
-    sum_result = sum_list(numbers)
+    sum_result = sum_list(num)
+    product_result = product_list(num)
+    reverse_result = reverse_list(num)
 
     print("Sum: ", sum_result)
     print("Product: ", product_result)
+    print("Reverse: ", reverse_result)
 
 if __name__ == "__main__":
     main()
