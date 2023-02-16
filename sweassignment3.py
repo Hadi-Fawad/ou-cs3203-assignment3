f0b87e06e07eef37a0937a0c70f7e58f5c4eefa5
Hadi-Fawad <hadi.fawad@ou.edu>
2023-02-15 21:18:33 -0600

    Second update to file, added multiplication ability

--- sweassignment3.py
+++ sweassignment3.py
@@ -1,9 +1,15 @@
-
-
-# function
 def sum_list(num):
     return sum(num)
-array = [1, 2, 3, 5, 10]
-result = sum_list(array)
-print(result)
 
+def product_list(num):
+    product = 1
+    for number in num:
+        product *= number
+    return product
+
+
+array = [1, 2, 3, 5, 10]
+sumres = sum_list(array)
+prodres = product_list(array)
+print(sumres)
+print(prodres)
